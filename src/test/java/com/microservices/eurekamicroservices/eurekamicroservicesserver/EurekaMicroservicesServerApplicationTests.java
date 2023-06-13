0ba7316917bce8466e975c34c0f0d7716c4252c8
unknown <nath.abhijit1990@gmail.com>
2023-06-14 01:37:30 +0530

    initial commit

---
+++ src/test/java/com/microservices/eurekamicroservices/eurekamicroservicesserver/EurekaMicroservicesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.eurekamicroservices.eurekamicroservicesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMicroservicesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
